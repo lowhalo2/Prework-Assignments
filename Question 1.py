8ed64a3061fdf19435c8fd6b5d5f3a0a2aea38d2
Carlos Puentes <unconfigured@null.spigotmc.org>
2023-01-19 18:17:49 -0500

    Added input to question 1

--- Question 1.py
+++ Question 1.py
@@ -2,4 +2,4 @@ def hello_name(user_name):
     """Prints a greeting using the username input"""
     print(f"hello_{user_name.upper()}!")
 
-hello_name('Carlos Puentes')
+hello_name(input('Enter your username: '))
